47012d421fdfa008fd532f375db79b1e7cabb6e6
04shr <113576539+04shr@users.noreply.github.com>
2025-02-07 13:20:46 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,23 +1,16 @@
-function startMouseTracking() {
-    fetch("http://your-server-ip:5000/run_streamlit")
+function openApp(appName) {
+    fetch(`/streamlit/${appName}`)
         .then(response => response.json())
-        .then(data => window.open("http://your-server-ip:8501", "_blank"))
+        .then(data => {
+            if (data.url) {
+                window.open(data.url, "_blank"); // Open in a new tab
+            } else {
+                console.error("Error:", data.error);
+            }
+        })
         .catch(error => console.error("Error:", error));
 }
 
-function startVoiceRecognition() {
-    fetch("http://your-server-ip:5000/run_streamlit_voice")
-        .then(response => response.json())
-        .then(data => window.open("http://your-server-ip:8502", "_blank"))
-        .catch(error => console.error("Error:", error));
-}
-
-function startScreenReader() {
-    fetch("http://your-server-ip:5000/run_streamlit_reader")
-        .then(response => response.json())
-        .then(data => window.open("http://your-server-ip:8503", "_blank"))
-        .catch(error => console.error("Error:", error));
-}
 
 function quiz() {
   window.open("quiz.html", "_blank");
